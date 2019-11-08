e0a7e70cd2e1ad87afebcd6f2dd5e29556671a92
rflowturing <wolf.reip@gmail.com>
2019-11-08 09:19:23 +0100

    ok

--- signup.php
+++ signup.php
@@ -20,7 +20,6 @@ if (isset($_POST['submit'])) {
         $pdo = new PDO("mysql:host=$dbhost;dbname=$db", $dbuser, $dbpass);
         $insertmbr = $pdo->prepare("INSERT INTO Student (first_name,last_name,username, email, password,linkedin , github) VALUES( ?,?,?,?,?,?,?)");
         $insertmbr->execute(array($first_name, $last_name, $username, $mail, $pw, $linkedin, $github));
-
         $good = "You have successfully created a new account!  <a href=\"signin.php\">Me connecter</a>";
     } else {
         $error = "Missing informations! please fill all the fields.";
@@ -46,14 +45,11 @@ if (isset($_POST['submit'])) {
 <body>
     <div class="signup container">
         <h1>Sign <span class="up">up</span> !</h1>
-
         <div class="content col-12 ">
-
             <form method="post" action="signup.php" name="formulaire">
                 <div class="row contReg">
                     <div class="col-4">
                         <div class="form-group">
-
                             <label for="firstname">First Name</label>
                             <input type="text" class="form-control" name="first_name" id="first_name" placeholder="Enter first name">
                         </div>
@@ -61,7 +57,6 @@ if (isset($_POST['submit'])) {
                             <label for="firstname">Last Name</label>
                             <input type="text" class="form-control" name="last_name" id="last_name" placeholder="Enter last name">
                         </div>
-
                         <div class="form-group">
                             <label for="exampleInputPassword1">Password</label>
                             <input type="password" class="form-control" name="password" id="exampleInputPassword1" placeholder="Password">
@@ -70,43 +65,32 @@ if (isset($_POST['submit'])) {
                             <label for="passwordVal">Password validation</label>
                             <input type="password" class="form-control" name="passwordval" id="passwordVal" placeholder="Password">
                         </div>
-
                     </div>
                     <div class="col-4">
                         <div class="form-group">
                             <label for="usename">Username</label>
                             <input type="text" class="form-control" name="username" id="usename" aria-describedby="emailHelp" placeholder="Enter username">
                         </div>
-
-
                         <div class="form-group">
                             <label for="exampleInputEmail1">Email address</label>
                             <input type="email" class="form-control" name="mail" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-
                         </div>
-
                         <div class="form-group">
                             <label for="linkedin">Linkedin</label>
                             <input type="text" class="form-control" name="linkedin" id="linkedin" placeholder="Enter your linkedin">
                         </div>
-
                         <div class="form-group">
                             <label for="github">Github</label>
                             <input type="text" class="form-control" name="github" id="github" placeholder="Enter your github">
                         </div>
                     </div>
                 </div>
-
+                <div class="but">
+                    <button align="center" type="submit" name="submit" class="btn btn-primary btnreg">Register</button>
+                </div>
             </form>
-
-        </div>
-        <div class="but">
-            <button align="center" type="submit" name="submit" class="btn btn-primary btnreg">Register</button>
         </div>
     </div>
-
-
-
     <div class="">
         <?php
         if (isset($error)) {
@@ -116,10 +100,6 @@ if (isset($_POST['submit'])) {
         }
         ?>
     </div>
-
-
-
-
 </body>
 
 </html>
